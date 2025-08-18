deb442179d6dc253b397ba4242609027167bc654
Krishna Chhabra <krishcarbon7@gmail.com>
2025-08-19 00:53:59 +0530

    Update q4B.cpp

--- q4B.cpp
+++ q4B.cpp
@@ -1,2 +1,58 @@
 // 4) Implement the logic to
 // b. Find the matrix multiplication
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int r1, c1, r2, c2;
+
+    cout << "Enter rows and columns of first matrix: ";
+    cin >> r1 >> c1;
+
+    cout << "Enter rows and columns of second matrix: ";
+    cin >> r2 >> c2;
+
+    if (c1 != r2) {
+        cout << "Matrix multiplication not possible! (Columns of first != Rows of second)" << endl;
+        return 0;
+    }
+
+    // Input matrices
+    vector<vector<int>> A(r1, vector<int>(c1));
+    vector<vector<int>> B(r2, vector<int>(c2));
+    vector<vector<int>> C(r1, vector<int>(c2, 0));
+
+    cout << "Enter elements of first matrix:" << endl;
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Enter elements of second matrix:" << endl;
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    // Matrix multiplication
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) { 
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    cout<<endl;
+    cout << " Resultant Matrix after multiplication :- " << endl;
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
